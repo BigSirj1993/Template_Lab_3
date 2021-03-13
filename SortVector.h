7d4bc6668a70de8efd068cc5995f186dd854a432
BigSirj1993 <77455531+BigSirj1993@users.noreply.github.com>
2021-03-12 20:16:44 -0600

    Create SortVector.h

---
+++ SortVector.h
@@ -0,0 +1,7 @@
+#pragma once
+#include "SimpleVector.h"
+
+// Lab 3: 3. Write a class template named SortVector.
+// Class should derive from SimpleVector from above. 
+// SortVector should do following:
+// Implement sort function, takes no parameters, will sort array in ascending order
